6b3d3685caa05d3df4e030bec6652ab631284381
Ben <ben@talkingquickly.co.uk>
2021-03-17 23:46:14 +0000

    Adds: regenerated configuration

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,16 +4,18 @@ set :branch, "master"
 # This is used in the Nginx VirtualHost to specify which domains
 # the app should appear on. If you don't yet have DNS setup, you'll
 # need to create entries in your local Hosts file for testing.
-set :nginx_server_name, "rdr_rails6_example.staging.talkingquickly.co.uk"
+set :nginx_server_name, 'rdr_rails6_example.staging.talkingquickly.co.uk'
 
 # used in case we're deploying multiple versions of the same
 # app side by side. Also provides quick sanity checks when looking
 # at filepaths
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 
+
 # Name sidekiq systemd service after the app and stage name so that
 # multiple apps and stages can co-exist on the same machine if needed
 set :sidekiq_service_unit_name, "#{fetch(:full_app_name)}_sidekiq"
+
 
 server '65.21.54.211', user: 'deploy', roles: %w{web app db}, primary: true
 
